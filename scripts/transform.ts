05331bef4e724d438131ea3ea5fcf8df4a6c0ee5
Jonathan Berger <drskullster@pm.me>
2023-11-17 10:30:11 +0100

    Add token transformation (#1)
    Add tokens, transform into esm module

---
+++ scripts/transform.ts
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const StyleDictionary = require('style-dictionary').extend('config.json5');
+
+import {
+	Dictionary,
+	File,
+	TransformedToken,
+	TransformedTokens,
+} from 'style-dictionary';
+const { fileHeader, getTypeScriptType } = StyleDictionary.formatHelpers;
+
+StyleDictionary.registerFormat({
+	name: 'javascript/esm',
+	formatter: ({ dictionary, file }: { dictionary: Dictionary; file: File }) => {
+		const newTokens = flattenDefaultTokens(dictionary.allTokens);
+
+		let result = fileHeader({ file });
+
+		// split the tokens into categories so they're exported separately
+		Object.keys(newTokens).forEach((tokenKey) => {
+			result += `export const ${tokenKey} = ${JSON.stringify(
+				newTokens[tokenKey],
+				null,
+				2,
+			)};\n\n`;
+		});
+
+		// Add a default export with all the token categories
+		result += `export default {\n${Object.keys(newTokens).join(',\n')}};`;
+
+		return result;
+	},
+});
+
+StyleDictionary.registerFormat({
+	name: 'typescript/esm-declarations',
+	formatter: ({ dictionary, file }: { dictionary: Dictionary; file: File }) => {
+		const newTokens = flattenDefaultTokens(dictionary.allTokens, true);
+
+		let result = fileHeader({ file });
+
+		// split the tokens into categories so they're exported separately
+		Object.keys(newTokens).forEach((tokenKey) => {
+			result += `export const ${tokenKey}: ${JSON.stringify(
+				newTokens[tokenKey],
+				null,
+				2,
+			)};\n\n`;
+		});
+
+		// TODO find a smarter way to remove quotes, this is fragile.
+		return result.replace(/"/g, '');
+	},
+});
+
+/**
+ * Remove the 'default' path from the tokens and strip the
+ * tokens from everything except `value`
+ */
+export function flattenDefaultTokens(
+	tokens: TransformedToken[],
+	isTSDeclaration: boolean = false,
+): TransformedTokens {
+	return tokens.reduce((acc: any, token: TransformedToken) => {
+		const newPath = token.path.filter((pathPart) => pathPart !== '_');
+		return addTokenToObject(acc, newPath, token, isTSDeclaration);
+	}, {});
+}
+
+/**
+ * Add token to the object based on the path
+ */
+function addTokenToObject(
+	obj: TransformedTokens,
+	path: string[],
+	token: TransformedToken,
+	isTSDeclaration: boolean = false,
+): TransformedTokens {
+	let currentObj: TransformedTokens | TransformedToken = obj || {};
+
+	for (let i = 0; i < path.length; i++) {
+		currentObj[path[i]] = currentObj[path[i]] || {};
+
+		currentObj = currentObj[path[i]];
+
+		if (i === path.length - 1) {
+			currentObj.value = isTSDeclaration
+				? getTypeScriptType(token.value)
+				: token.value;
+			currentObj.name = isTSDeclaration ? 'string' : token.name;
+		}
+	}
+
+	return obj;
+}
+
+StyleDictionary.buildAllPlatforms();
---
+++ tests/transform.spec.ts
@@ -0,0 +1,66 @@
+import { expect, test } from 'vitest';
+import { flattenDefaultTokens } from '../scripts/transform';
+import { TransformedToken } from 'style-dictionary';
+
+const sampleTokens: TransformedToken[] = [
+	{
+		name: 'Background',
+		path: ['color', 'bg', '_'],
+		value: '#ffffff',
+		isSource: false,
+		original: null,
+		filePath: '',
+	},
+	{
+		name: 'BackgroundSubtle',
+		path: ['color', 'bg', 'subtle'],
+		value: '#eeeeee',
+		isSource: false,
+		original: null,
+		filePath: '',
+	},
+	{
+		name: 'Spacing',
+		path: ['spacing', '_'],
+		value: 16,
+		isSource: false,
+		original: null,
+		filePath: '',
+	},
+];
+
+test('Flatten default tokens', () => {
+	expect(flattenDefaultTokens(sampleTokens)).toEqual({
+		color: {
+			bg: {
+				name: 'Background',
+				value: '#ffffff',
+				subtle: {
+					name: 'BackgroundSubtle',
+					value: '#eeeeee',
+				},
+			},
+		},
+		spacing: {
+			name: 'Spacing',
+			value: 16,
+		},
+	});
+
+	expect(flattenDefaultTokens(sampleTokens, true)).toEqual({
+		color: {
+			bg: {
+				name: 'string',
+				value: 'string',
+				subtle: {
+					name: 'string',
+					value: 'string',
+				},
+			},
+		},
+		spacing: {
+			name: 'string',
+			value: 'number',
+		},
+	});
+});
